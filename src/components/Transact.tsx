81baa1e28ae097491a5ce5065d6cd3c4005f9e4b
Nith567 <nithinkatkam504106@gmail.com>
2024-06-09 19:21:55 +0530

    Update Transact.tsx

--- src/components/Transact.tsx
+++ src/components/Transact.tsx
@@ -5,8 +5,8 @@ import { useState } from "react";
 import { myNFTABI, myNFTAddress } from "@/ABIs/myNFT";
 import { ethers } from "ethers";
 
-  let encodedBytes='0x0000000000000000000000008879318091671ba1274e751f8cdef76bb37eb3ed00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000d53756d6d657220436861696e2100000000000000000000000000000000000000'
-// example batch transaction, making two mint NFT calls
+  let encodedBytes='0x000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000600000000000000000000000008879318091671ba1274e751f8cdef76bb37eb3ed000000000000000000000000000000000000000000000000000000000000000d53756d6d657220436861696e2100000000000000000000000000000000000000'
+
 export function Transact() {
   const account = useAccount();
   const [id, setId] = useState<string | undefined>(undefined);
@@ -14,21 +14,20 @@ export function Transact() {
     mutation: { onSuccess: (id) => setId(id) },
   });
 
-
-
   const encodeData=async()=>{
     const mintArguments = {
-      mintToAddress: "0x8879318091671ba1274e751f8cdef76bb37eb3ed",
       quantityToMint: 1,
       mintComment: "Summer Chain!",
+      mintReferral: "0x8879318091671ba1274e751f8cdef76bb37eb3ed"
     };
  
+ 
     // Define the types and values in the same order as the struct definition
-    const types = ["address", "uint256", "string"];
+    const types = ["uint256", "string", "address"];
     const values = [
-      mintArguments.mintToAddress,
       mintArguments.quantityToMint,
-      mintArguments.mintComment
+      mintArguments.mintComment,
+      mintArguments.mintReferral
     ];
     
     // Encode the parameters using ethers.js
@@ -51,7 +50,14 @@ export function Transact() {
                   address: myNFTAddress,
                   abi: myNFTABI,
                   functionName: "mint",
-                  args: ["0xeb334f3fbd826ce99f1e74d7d074fbe351f4157a", 1, 1, ["0x8879318091671ba1274e751f8cdef76bb37eb3ed"],encodedBytes ]
+                  // args: ["0xeb334f3fbd826ce99f1e74d7d074fbe351f4157a", 1, 1, ["0x8879318091671ba1274e751f8cdef76bb37eb3ed"],encodedBytes ]
+                  args: [
+                    "0xeb334f3fbd826ce99f1e74d7d074fbe351f4157a", // minter
+                    1, // tokenId
+                    1, // quantity
+                    ["0x8879318091671ba1274e751f8cdef76bb37eb3ed"], // rewardsRecipients
+                    encodedBytes // minterArguments
+                  ],
                 }
               ],
             });
